05c8604d36f4e69751505d24d7ac366a8c5eca26
Sang <sangbui1281@gmail.com>
2020-12-23 16:03:49 -0700

    Insertion Sort implemented

---
+++ Sorting Algorithms/Insertion Sort/InsertionSort.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+import java.util.ArrayList;
+
+
+class InsertionSort{
+    public static void main(String[] args) {
+        Scanner kb = new Scanner(System.in);
+        int numOfNums = kb.nextInt();
+        ArrayList<Integer> numbers = new ArrayList<Integer>();
+        for(int i = 1; i<=numOfNums; i++)
+            numbers.add(kb.nextInt());
+        for(int i = 0; i<numOfNums; i++){
+            int j = i;
+            while(j>0 && numbers.get(j) < numbers.get(j-1)){
+                // Swap positions.
+                int temp = numbers.get(j);
+                numbers.set(j, numbers.get(j-1));
+                numbers.set(j-1, temp);
+                j--;
+            }
+        }
+        System.out.println(numbers);
+    }
+}
